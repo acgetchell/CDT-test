658fea042dc23c0af4080a6702c3aae65878e601
Adam Getchell <acgetchell@ucdavis.edu>
2019-10-18 11:47:13 -0700

    Fix currentDateTime function
    100 tests passing.

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -116,22 +116,14 @@ inline std::ostream& operator<<(std::ostream& os, topology_type const& topology)
 //}
 
 /// @brief Return the current date and time
-///
-/// **Auto** doesn't work here as a return type because **time_str** is a
-/// stack memory address.
-///
-/// @return The current data and time in a thread-safe manner using
-/// **localtime_r()** as a std::string.
-inline const std::string currentDateTime() noexcept {
-  auto now = time(0);
-  struct tm tstruct;
-  char time_str[100];
-  localtime_r(&now, &tstruct);
-  // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
-  // for more info about date/time format
-  strftime(time_str, sizeof(time_str), "%Y-%m-%d.%X%Z", &tstruct);
-
-  return time_str;
+inline const std::string currentDateTime() {
+  std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+  std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+  auto result_c = std::put_time(std::localtime(&now_c), "%Y-%m-%d.%X%Z");
+  std::ostringstream result_s;
+  result_s << result_c;
+  std::string result = result_s.str();
+  return result;
 }
 
 /// @brief  Generate useful filenames
